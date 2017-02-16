084ece048275af00a47b7d392d2e156c35ede287
Ashraf <Ashrafnet@outlook.com>
2017-02-16 11:47:43 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace LogoDetector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Folder";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.textBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystemDirectories;
+            this.textBox1.Location = new System.Drawing.Point(64, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(528, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "C:\\D\\Ken\\Watermark Detection\\Photos";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(598, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Proccess";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(397, 213);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.FullRowSelect = true;
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(260, 303);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Image";
+            this.columnHeader1.Width = 90;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Has Logo";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Process Time";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "1487183617_11.png");
+            this.imageList1.Images.SetKeyName(1, "1487183658_17.png");
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 32);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(661, 303);
+            this.splitContainer1.SplitterDistance = 260;
+            this.splitContainer1.TabIndex = 6;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.pictureBox1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.pictureBox2);
+            this.splitContainer2.Size = new System.Drawing.Size(397, 303);
+            this.splitContainer2.SplitterDistance = 213;
+            this.splitContainer2.TabIndex = 5;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(397, 86);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 347);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Bitmap Contains";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,441 @@
+﻿using LogoDetector.Properties;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LogoDetector
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            listView1.SuspendLayout();
+            string folderPath = textBox1.Text;
+            double  total_process_time = 0;
+            long _cnt = 0;
+            var s = System.Diagnostics.Stopwatch.StartNew();
+            try
+            {
+                var imgExts = new string[] { "*.jpeg", "*.jpg", "*.png", "*.BMP", "*.GIF", "*.TIFF", "*.Exif","*.WMF", "*.EMF" };
+                // foreach (var item in MyDirectory.GetFiles(textBox1.Text, imgExts, SearchOption.AllDirectories))
+
+                Task task = Task.Factory.StartNew(delegate
+                {
+                    Parallel.ForEach(MyDirectory.GetFiles(folderPath, imgExts, SearchOption.AllDirectories), new ParallelOptions { MaxDegreeOfParallelism = 8 }, item =>
+              {
+                  ListViewItem lvi = null;
+                  ImageLogoInfo info = null;
+                  if (File.Exists(item))
+                  {
+
+
+
+                       info = ImageLogoInfo.ProccessImage(item);
+                      
+                      lvi = new ListViewItem(info.ImageName, info.HasLogo ? 0 : 1);
+
+                      lvi.SubItems.Add(info.HasLogo ? "Yes" : "No");
+                      lvi.SubItems.Add(info.ProcessingTime + " ms");
+                      total_process_time += info.ProcessingTime;
+                      lvi.Tag = info;
+                      _cnt++;
+                  }
+                  else
+                  {
+
+                      lvi = new ListViewItem(Path.GetFileName(item), 1);
+                      lvi.SubItems.Add("File does not exist!");
+                      lvi.BackColor = Color.Red;
+                      lvi.ForeColor = Color.White;
+
+                  }
+
+                  BeginInvoke((Action)(() =>
+                  {
+                      if (info.HasLogo)
+                          listView1.Items.Add(lvi);
+                      Text = s.Elapsed.TotalSeconds + " Seconds" + " [Total Process Time: " + total_process_time / 1000 + " Seconds]" + " (" + _cnt + " Items)";
+                  }));
+                
+
+              }
+                 );
+                });
+                task.ContinueWith((t) =>
+        BeginInvoke((Action)(() =>
+        {
+            s.Stop();
+            Text = s.Elapsed.TotalSeconds + " Seconds" + " [Total Process Time: " + total_process_time / 1000 + " Seconds]" + " (" + _cnt + " Items)";
+            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            listView1.ResumeLayout();
+        }))
+);
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"error", MessageBoxButtons.OK, MessageBoxIcon.Error );
+            }
+            finally
+            {
+                
+            }
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 1)
+            {
+                
+                var info = (ImageLogoInfo)listView1.SelectedItems[0].Tag;
+                if (info == null)
+                {
+                    pictureBox1.Image =
+                    pictureBox2.Image = null;
+                }
+                else
+                {
+                    ImageLogoInfo info1 = ImageLogoInfo.ProccessImage(info.ImagePath );
+                    pictureBox1.Image = (Bitmap)Bitmap.FromStream(new MemoryStream(File.ReadAllBytes(info.ImagePath)));
+                    pictureBox2.Image = info.ProcessedImage ;
+                }
+
+            }
+        }
+    }
+
+
+    class ControlInvikerHelper
+    {
+        delegate void UniversalVoidDelegate();
+
+        /// <summary>
+        /// Call form control action from different thread
+        /// </summary>
+        public static void ControlInvike(Control control, Action function)
+        {
+            if (control.IsDisposed || control.Disposing)
+                return;
+
+            if (control.InvokeRequired)
+                control.Invoke(new UniversalVoidDelegate(() => ControlInvike(control, function)));
+            else
+                function();
+        }
+    }
+
+
+    public static class BitmapProcess
+    {
+
+
+        public static bool IsCloseColor(Color color1, Color color2)
+        {
+            return Math.Abs(color1.R - color2.R) < 20 && Math.Abs(color1.G - color2.G) < 20 && Math.Abs(color1.B - color2.B) < 20;
+        }
+        /// <summary>
+        /// Resize the image
+        /// </summary>
+        public static Bitmap GetLastWidthHeight(Bitmap bitmap, int Width, int Height)
+        {
+            var x = Math.Max(0, bitmap.Width - Width);
+            var y = Math.Max(0, bitmap.Height - Height);
+            Width = Math.Min(Width, bitmap.Width);
+            Height = Math.Min(Height, bitmap.Height);
+            return bitmap.Clone(new Rectangle(x, y, Width, Height), bitmap.PixelFormat);
+
+        }
+
+
+        /// <summary>
+        /// Finds all areas with this color
+        /// </summary>
+        public static List<Point> FillPath(BitmapPixels bitmap, int X, int Y)
+        {
+            List<Point> path = new List<Point>();
+            Queue q = new Queue();
+            path.Add(new Point(X, Y));
+            q.Enqueue(new Point(X, Y));
+            var pixelColor = bitmap[X, Y];
+            while (q.Count > 0)
+            {
+                var p = (Point)q.Dequeue();
+                X = p.X;
+                Y = p.Y;
+                for (int i = 1; i <= 4; i++)
+                {
+                    int x2 = X;
+                    int y2 = Y;
+                    switch (i)
+                    {
+                        case 1://left
+                            x2--;
+                            break;
+                        case 2://Right
+                            x2++;
+                            break;
+                        case 3://down
+                            y2++;
+                            break;
+                        case 4://up
+                            y2--;
+                            break;
+                    }
+                    if (x2 < bitmap.Width && x2 >= 0 && y2 < bitmap.Height && y2 >= 0)
+                    {
+                        var p2 = new Point(x2, y2);
+                        if (!path.Contains(p2) && bitmap[x2, y2] == pixelColor)
+                        {
+                            q.Enqueue(p2);
+                            path.Add(p2);
+                        }
+                    }
+
+                }
+            }
+            return path;
+        }
+        /// <summary>
+        /// Finds all closed paths with the same color
+        /// </summary>
+        /// <param name="bitmap"></param>
+        /// <returns></returns>
+        public static List<List<Point>> FindClosedPaths(BitmapPixels bitmap, int PathPixelCount)
+        {
+            var paths = new List<List<Point>>();
+            var pixels = bitmap.NotbackgroundPixels.ToList();
+
+            while (pixels.Count > 0)
+            {
+                var path = FillPath(bitmap, pixels[0].X, pixels[0].Y);
+                if (path.Count >= PathPixelCount)
+                    paths.Add(path);
+                pixels = pixels.Except(path).ToList();
+            }
+            return paths;
+        }
+        /// <summary>
+        /// Calc the distance between the two points
+        /// </summary>
+        /// <returns></returns>
+        public static double Distance(Point p1, Point p2)
+        {
+            return Math.Sqrt(Math.Pow(p2.X - p1.X, 2) + Math.Pow(p2.Y - p1.Y, 2));
+        }
+        /// <summary>
+        /// Finds the distance between edges
+        /// </summary>
+        /// <param name="bitmap"></param>
+        /// <returns></returns>
+        public static double[] CalcEdgesQatars(List<Point> Path)
+        {
+            var p1 = (from p in Path orderby p.X ascending select p).First();
+            var p2 = (from p in Path orderby p.X descending select p).First();
+            var p3 = (from p in Path orderby p.Y ascending select p).First();
+            var p4 = (from p in Path orderby p.Y descending select p).First();
+
+            var distances = new List<double>();
+            var d1 = Distance(p1, p2);
+            var d2 = Distance(p3, p4);
+            return new double[] { d1, d2 };
+        }
+
+        public static bool HasLogo(Bitmap bitmap)
+        {
+            var pixels = new BitmapPixels(bitmap);
+            var closedPaths = BitmapProcess.FindClosedPaths(pixels, 30);
+
+            var repeated = BitmapProcess.CalculateTheRepeatedPathsCount(closedPaths.ConvertAll(c => c.Count), 15);
+            if (repeated.Count == 0 || repeated[0] < 3)
+                return false;
+
+            var distances = closedPaths.ConvertAll(c => CalcEdgesQatars(c));
+            var sameDistance = new List<int>();
+
+            foreach (var item1 in distances)
+            {
+                var counter = 0;
+                foreach (var item2 in distances)
+                {
+                    var c1 = item2[0] < 20 && item2[1] < 20 && Math.Abs(item2[0] - item2[1]) < 10;
+                    var c2 = Math.Abs(item1[0] - item2[0]) < 5 || Math.Abs(item1[0] - item2[1]) < 5;
+                    var c3 = Math.Abs(item1[1] - item2[0]) < 5 || Math.Abs(item1[1] - item2[1]) < 5;
+                    if (c1 && c2 && c3) counter++;
+                }
+                sameDistance.Add(counter);
+            }
+            sameDistance.Sort();
+            return sameDistance.Last() >= 3;
+        }
+        /// <summary>
+        /// Mark the path
+        /// </summary>
+        /// <param name="bitmap"></param>
+        /// <returns></returns>
+        public static void MarkPath(Bitmap bitmap, List<Point> Path, Color Color)
+        {
+            foreach (var item in Path)
+                bitmap.SetPixel(item.X, item.Y, Color);
+        }
+        /// <summary>
+        /// Finds how many each number repeated
+        /// </summary>
+        /// <param name="Counts"></param>
+        /// <returns></returns>
+        public static List<int> CalculateTheRepeatedPathsCount(List<int> Counts, int Threshold)
+        {
+            var list = new List<int>();
+            foreach (var item in Counts)
+            {
+                var n = Counts.Count(c =>
+                {
+                    var diff = Math.Abs(c - item);
+                    return diff <= Threshold;
+                });
+                if (!list.Contains(n))
+                    list.Add(n);
+            }
+            list.Sort();
+            list.Reverse();
+            return list;
+        }
+
+    }
+
+    public class BitmapPixels
+    {
+        /// <summary>
+        /// If Color is backgroun then True
+        /// </summary>
+        public bool[,] Colors { get; private set; }
+        /// <summary>
+        /// The pixels other than background
+        /// </summary>
+        public List<Point> NotbackgroundPixels { get; private set; }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public BitmapPixels(Bitmap bitmap)
+        {
+            this.Width = bitmap.Width;
+            this.Height = bitmap.Height;
+            Colors = new bool[Width, Height];
+            NotbackgroundPixels = new List<Point>();
+            caclulate(bitmap);
+        }
+
+        public bool this[int X, int Y]
+        {
+            get { return Colors[X, Y]; }
+            set { Colors[X, Y] = value; }
+        }
+
+
+
+        private void caclulate(Bitmap bitmap)
+        {
+            var backgroundColors = new List<Color>();
+            for (int i = 1; i < 30; i++)
+            {
+                var color = bitmap.GetPixel(bitmap.Width - i, bitmap.Height - i);
+                var count = backgroundColors.Count;
+                if (count == 0 || BitmapProcess.IsCloseColor(color, backgroundColors[count - 1]))
+                    backgroundColors.Add(color);
+            }
+
+            for (int i = bitmap.Width - 1; i >= 0; i--)
+            {
+                for (int j = bitmap.Height - 1; j >= 0; j--)
+                {
+                    var color = bitmap.GetPixel(i, j);
+                    Colors[i, j] = backgroundColors.Exists(c => BitmapProcess.IsCloseColor(c, color));
+                    if (!Colors[i, j])
+                        NotbackgroundPixels.Add(new Point(i, j));
+                }
+            }
+        }
+
+    }
+    public static class MyDirectory
+    {   // Regex version
+        public static IEnumerable<string> GetFiles(string path,
+                            string searchPatternExpression = "",
+                            SearchOption searchOption = SearchOption.TopDirectoryOnly)
+        {
+            Regex reSearchPattern = new Regex(searchPatternExpression, RegexOptions.IgnoreCase);
+            return Directory.EnumerateFiles(path, "*", searchOption)
+                            .Where(file =>
+                                     reSearchPattern.IsMatch(Path.GetExtension(file)));
+        }
+
+        // Takes same patterns, and executes in parallel
+        public static IEnumerable<string> GetFiles(string path,
+                            string[] searchPatterns,
+                            SearchOption searchOption = SearchOption.TopDirectoryOnly)
+        {
+            return searchPatterns.AsParallel()
+                   .SelectMany(searchPattern =>
+                          Directory.EnumerateFiles(path, searchPattern, searchOption));
+        }
+    }
+
+    /// <summary>
+    /// This class just holds the image info after we process it.
+    /// </summary>
+    class ImageLogoInfo
+    {
+        public long ProcessingTime { get; set; }
+        public string ImagePath { get; set; }
+        public string ImageName { get
+            {
+                if (string.IsNullOrWhiteSpace(ImagePath))
+                    return null;
+                else
+                    return Path.GetFileName(ImagePath);
+            }
+        }
+        public bool  HasLogo { get; set; }
+        public Bitmap ProcessedImage { get; set; }
+
+        public static ImageLogoInfo ProccessImage(string imgPath)
+        {
+            ImageLogoInfo info = new ImageLogoInfo();
+            info.ImagePath = imgPath;
+            Bitmap bitmap = (Bitmap)Bitmap.FromStream(new MemoryStream(File.ReadAllBytes(imgPath)));
+            var sw = System.Diagnostics.Stopwatch.StartNew();
+            //  bitmap = BitmapProcess.ReSizeImage(bitmap, 60, 60); // crop the right button image
+
+             
+              bitmap = BitmapProcess.GetLastWidthHeight(bitmap, 70, 70); // crop the right button image
+           // var pixles = new BitmapPixels(bitmap);// BitmapProcess.MarkImage(bitmap);
+           // var closedPaths = BitmapProcess.FindClosedPaths(pixles , 50);
+           // var repeated = BitmapProcess.CalculateTheRepeatedPathsCount(closedPaths.ConvertAll(c => c.Count), 15);
+            info.HasLogo= BitmapProcess.HasLogo(bitmap);
+            info.ProcessedImage = bitmap;
+           // info.HasLogo = repeated.Count > 0 && repeated[0] >= 3;
+            sw.Stop();
+            info.ProcessingTime = sw.ElapsedMilliseconds;
+
+            return info;
+        }
+
+    }
+}
